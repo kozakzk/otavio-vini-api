41c628da64db3398e23fb51a5e84475a0a2ba26e
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2026-01-20 13:59:22 -0300

    feature: add login and register page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { ElectronicsList } from './routes/ElectronicsList';
+import { Login } from './routes/Login';
+import { Register } from './routes/Register';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="list" element={<ElectronicsList />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/routes/Register.tsx
@@ -0,0 +1,78 @@
+import {
+  Button,
+  Link as ChakraLink,
+  Checkbox,
+  Flex,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Link,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { AtSign, Lock } from 'lucide-react';
+import { Link as RouterLink, useNavigate } from 'react-router';
+
+export function Register() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Flex
+        justifyContent={'center'}
+        alignItems={'center'}
+        height={'100svh'}
+        padding={{ base: '2em', md: '0' }}
+      >
+        <VStack spacing={10}>
+          <Text fontSize={'5xl'} color={'gray.600'} align={'center'}>
+            Cadastrar
+          </Text>
+          <VStack spacing={2} width={'100%'}>
+            <InputGroup>
+              <Input placeholder={'Email'} type={'email'} />
+              <InputRightElement>
+                <AtSign color={'gray'} />
+              </InputRightElement>
+            </InputGroup>
+            <InputGroup>
+              <Input placeholder={'Senha'} type={'password'} />
+              <InputRightElement>
+                <Lock color={'gray'} />
+              </InputRightElement>
+            </InputGroup>
+            <InputGroup>
+              <Input placeholder={'Repita sua senha'} type={'password'} />
+              <InputRightElement>
+                <Lock color={'gray'} />
+              </InputRightElement>
+            </InputGroup>
+          </VStack>
+          <Flex
+            flexDirection={{ base: 'column', sm: 'row' }}
+            justifyContent={'space-between'}
+            width={'100%'}
+            align={'center'}
+            textAlign={'center'}
+          >
+            <Checkbox>Lembre-se de Mim</Checkbox>
+            <Link color={'blue'}>Recuperar Senha</Link>
+          </Flex>
+          <Button
+            colorScheme={'blue'}
+            width={'100%'}
+            onClick={() => navigate('/list')}
+          >
+            Login
+          </Button>
+          <Text align={'center'}>
+            Já possui uma conta?{' '}
+            <ChakraLink as={RouterLink} to={'/login'} color={'blue'}>
+              Entrar
+            </ChakraLink>
+          </Text>
+        </VStack>
+      </Flex>
+    </>
+  );
+}

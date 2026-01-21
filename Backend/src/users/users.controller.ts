833763e8bb9f7a854c44a0cd23d83751278f0ff2
Otavio Kozak <o.kozak@edu.pucrs.br>
2026-01-21 09:20:43 -0300

    User controller e service em progresso. User DTOs criados

--- Backend/src/users/users.controller.ts
+++ Backend/src/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get() 
+    findAll() {
+        return []
+    }
+
+    @Get(':email') 
+    findOne(@Param('email') email: string) {
+        return { email }
+    }
+
+    @Post()
+    create(@Body() user: {}) {
+        return user
+    }
+
+    @Patch(':email') 
+    update(@Param('email') email: string, @Body() userUpdate: {}) {
+        return { email, ...userUpdate }
+    }
+
+    @Delete(':email') 
+    delete(@Param('email') email: string) {
+        return { email }
+    }
+
+}

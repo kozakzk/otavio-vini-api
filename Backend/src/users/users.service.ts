41b2573d07cdea1a4ed48583c5fc299f2df3d1f5
Otavio Kozak <o.kozak@edu.pucrs.br>
2026-01-21 09:20:43 -0300

    User controller e service em progresso. User DTOs criados

--- Backend/src/users/users.service.ts
+++ Backend/src/users/users.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "email": "fabricio@pnb.com",
+            "password": "paidetodos"
+        }
+    ]
+
+    async findAll() {
+        return this.users
+    }
+
+    async findOne(id: number) {
+        const user = this.users.find(user => user.id === id)
+
+        return user
+    }
+
+    async create() {
+        
+    }
+
+    async update() {
+        
+    }
+
+    async delete(id: number) {
+        const removedUser = this.findOne(id)
+
+        this.users = this.users.filter(user => user.id !== id)
+
+        return removedUser
+    }
+
+}

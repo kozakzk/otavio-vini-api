9d663e2612c8a13fe4ca2a8b395f6d08b8c3a18c
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2026-01-21 10:07:30 -0300

    feature: make electronics list page responsive (#4)

--- frontend/src/routes/ElectronicsList.tsx
+++ frontend/src/routes/ElectronicsList.tsx
@@ -5,37 +5,50 @@ import {
   CardFooter,
   Divider,
   Flex,
-  Heading,
   Image,
   Text,
+  VStack,
 } from '@chakra-ui/react';
+import { ShoppingCart } from 'lucide-react';
 import mockedElectronicsList from '../mocks/electronics-list.mock.json';
 
 export function ElectronicsList() {
   return (
     <>
-      <Heading textAlign={'center'} color={'blue.600'}>
+      <Text textAlign={'center'} color={'blue.600'} fontSize={'3rem'}>
         Lista de produtos
-      </Heading>
-      <Flex justifyContent={'space-evenly'}>
+      </Text>
+      <Flex
+        flexDirection={{ base: 'column', md: 'row' }}
+        justifyContent={'space-evenly'}
+        alignItems={'center'}
+      >
         {mockedElectronicsList.map((electronic) => (
           <Card
             maxW={'sm'}
+            width={'50rem'}
+            height={'40rem'}
             key={electronic.id}
             border={1}
             margin={'1em'}
             padding={'1em'}
           >
-            <Heading height={'20rem'} textOverflow={'ellipsis'}>
-              {electronic.name}
-            </Heading>
-            <CardBody>
-              <Image
-                src={electronic.imgUrl}
-                alt={electronic.name}
-                height={'25rem'}
-                draggable={false}
-              />
+            <Image
+              src={electronic.imgUrl}
+              alt={electronic.name}
+              draggable={false}
+              width={'100%'}
+            />
+            <VStack height={'10rem'}>
+              <Text
+                fontSize={'1.2rem'}
+                fontWeight={500}
+                textOverflow={'hidden'}
+              >
+                {electronic.name}
+              </Text>
+            </VStack>
+            <CardBody alignContent={'end'} maxH={'5rem'}>
               <Text color={'blue.600'} fontSize={'2xl'}>
                 {electronic.price.toLocaleString('pt-BR', {
                   style: 'currency',
@@ -45,7 +58,9 @@ export function ElectronicsList() {
               <Divider />
             </CardBody>
             <CardFooter justifyContent={'center'}>
-              <Button colorScheme={'blue'}>Comprar agora</Button>
+              <Button colorScheme={'blue'} leftIcon={<ShoppingCart />}>
+                Comprar agora
+              </Button>
             </CardFooter>
           </Card>
         ))}

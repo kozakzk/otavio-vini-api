1a50b95cb152545d82f2e7c372494e0304d2fe2e
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2026-01-26 09:42:04 -0300

    fix(products): protect the GET /products route (#11)

--- Backend/src/products/products.controller.ts
+++ Backend/src/products/products.controller.ts
@@ -1,11 +1,18 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductsService } from './products.service';
+import { UseGuards } from '@nestjs/common';
+import { Role } from 'src/auth/enums/role.enum';
+import { AuthGuard } from '@nestjs/passport';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @UseGuards()
+  @UseGuards(AuthGuard('jwt'))
+  @Roles(Role.Admin, Role.User)
   @Get()
   findAll() {
     return this.productsService.findAll();

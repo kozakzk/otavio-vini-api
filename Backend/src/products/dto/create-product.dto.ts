51e1c97b49a003d90b9e7d6614acdeaac27e79a8
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2026-01-26 09:26:45 -0300

    fix(swagger): add ApiProperty decorators to DTOs fields

--- Backend/src/products/dto/create-product.dto.ts
+++ Backend/src/products/dto/create-product.dto.ts
@@ -1,18 +1,23 @@
 import { IsNotEmpty, IsNumber, IsString, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProductDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
   price: number;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @IsUrl()

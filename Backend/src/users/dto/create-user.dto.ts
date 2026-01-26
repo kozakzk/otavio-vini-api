12f99de059b68fadb3b807bdfa75a7395fea0515
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2026-01-26 09:27:39 -0300

    fix(swagger): add ApiProperty decorators to DTOs fields (#10)

--- Backend/src/users/dto/create-user.dto.ts
+++ Backend/src/users/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { Role } from 'src/auth/enums/role.enum';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   role: Role = Role.User;

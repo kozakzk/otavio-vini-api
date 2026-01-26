4733f49fa9b8bfe6a4388d987497e7dfaf12c55d
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2026-01-26 12:38:28 -0300

    fix!(prisma): upgrade prisma to version 7 (#12)

--- Backend/src/prisma/prisma.service.ts
+++ Backend/src/prisma/prisma.service.ts
@@ -1,16 +1,13 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
+export class PrismaService extends PrismaClient {
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({ adapter });
   }
 }

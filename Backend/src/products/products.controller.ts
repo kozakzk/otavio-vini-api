b25f6b8ef94625a2b05c935a412f9dedfef257d1
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2026-01-27 10:47:52 -0300

    feature(frontend): add protected route

--- Backend/src/products/products.controller.ts
+++ Backend/src/products/products.controller.ts
@@ -10,9 +10,6 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @UseGuards()
-  @UseGuards(AuthGuard('jwt'))
-  @Roles(Role.Admin, Role.User)
   @Get()
   findAll() {
     return this.productsService.findAll();
